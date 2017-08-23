f3f1a2d1805e677147c1fb7077ccc5422576cd46
Renan GEHAN <rgehan94@gmail.com>
2017-08-24 00:20:45 +0200

    Fix search tests

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -3,35 +3,67 @@ import FuzzyFinder from '../fuzzyfinder';
 let ff = new FuzzyFinder('<', '>');
 
 /*
- * Boolean behavior
+ * Basic behavior
  */
 
 test("'foo' should match ['foobar', 'barbaz', 'foobaz']", () => {
-  expect(ff.search('foo', ['foobar', 'barbaz', 'foobaz'], false)).toEqual([
-    'foobar',
-    'foobaz'
+  expect(ff.search('foo', ['foobar', 'barbaz', 'foobaz'])).toMatchObject([
+    { text: 'foobar' },
+    { text: 'foobaz' },
   ]);
 });
 
 test("'foo' should not match ['barbaz', 'bazbar']", () => {
-  expect(ff.search('foo', ['barbaz', 'bazbar'], false)).toEqual([]);
+  expect(ff.search('foo', ['barbaz', 'bazbar'])).toEqual([]);
 });
 
 test("'foo' should not match []", () => {
-  expect(ff.search('foo', [], false)).toEqual([]);
+  expect(ff.search('foo', [])).toEqual([]);
 });
 
 /*
- * Highlighting behavior
+ * Complex behavior:
+ * - Object search subject & getter
+ * - Full output
  */
 
-test("'foo' should highlight ['foobar', 'barbaz', 'foobaz'] properly", () => {
-  expect(ff.search('foo', ['foobar', 'barbaz', 'foobaz'])).toEqual([
-    '<f><o><o>bar',
-    '<f><o><o>baz',
- ]);
+test("'baaa' should match [{ product: 'Banana', price: 1.23 }, { product: 'Meat', price: 10.67 }]", () => {
+  expect(ff.search('baaa', [
+    { product: 'Banana', price: 1.23 },
+    { product: 'Meat', price: 10.67 }
+  ], {
+    highlight: false,
+    outputFull: true,
+    getter: obj => obj.product,
+  })).toMatchObject([{
+    text: 'Banana',
+    subject: {
+      product: 'Banana',
+      price: 1.23,
+    },
+  }]);
 });
 
-test("'foo' should not highlight anything from ['barbaz', 'bazbar']", () => {
-  expect(ff.search('foo', ['barbaz', 'bazbar'])).toEqual([]);
+/*
+ * Complex behavior:
+ * - Object search subject & getter
+ * - Full output
+ * - Highlighting
+ */
+
+test("'baaa' should match & highlight [{ product: 'Banana', price: 1.23 }, { product: 'Meat', price: 10.67 }]", () => {
+  expect(ff.search('baaa', [
+    { product: 'Banana', price: 1.23 },
+    { product: 'Meat', price: 10.67 }
+  ], {
+    highlight: true,
+    outputFull: true,
+    getter: obj => obj.product,
+  })).toMatchObject([{
+    text: '<B><a>n<a>n<a>',
+    subject: {
+      product: 'Banana',
+      price: 1.23,
+    },
+  }]);
 });
